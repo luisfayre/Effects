f3734461b5cf74eb90381bd504c17f9dadb234bd
luisfayre <15551452@lc_047.itchii.lab>
2017-11-08 10:02:28 -0700

    Bugs
    as

--- www/js/scene.js
+++ www/js/scene.js
@@ -11,38 +11,38 @@
 	animeteScene();
 
 	function initializeScene() {
-		/*webGLAvaible = false;
-		if(Detector.webgl){
-			renderer = new.THREE.WebGLRenderer({antialias: true});
+		webGLAvaible = false;
+		
+			renderer = new THREE.WebGLRenderer({antialias: true});
 			webGLAvaible = true;
-		}else{
+		
 			renderer = new THREE.CanvasRenderer();
-			renderer.setClearColorHex(0*000000,1);
-		}*/
+			renderer.setClearColorHex(0x000000, 1);
+	
 		canvasWidth = window.innerWidth - 10;
 		canvasHeigth = window.innerHeigth - 20;
 
 		renderer.setSize(canvasWidth, canvasHeigth);
 
-		document.getElementsById("WebGLCanvas").appendChinld(renderer.domElement);
+		document.getElementById("WebGLCanvas").appendChild(renderer.domElement);
 
 		scene = new THREE.Scene();
 
-		camera = THREE.PerspectiveCamera(45,canvasWidth/canvasHeigth,1,100);
+		camera = new THREE.PerspectiveCamera(45,canvasWidth/canvasHeigth,1,100);
 		camera.position.set(0,0,15);
 		camera.lookAt(scene.position);
 		scene.add(camera);
 
-		startTexture = new THREE.ImageUtils.loadTexture("public/img/Star.jpg");
+		startTexture = new THREE.ImageUtils.loadTexture("public/Star.jpg");
 
 		for(i = 0; i< num; i++){
 			var squareGeometry = new THREE.Geometry();
-			squareGeometry.vertice.push(new Vector3(-1,-1,0));
-			squareGeometry.vertice.push(new Vector3(1,-1,0));
-			squareGeometry.vertice.push(new Vector3(1,1,0));
-			squareGeometry.vertice.push(new Vector3(-1,1,0));
+			squareGeometry.vertices.push(new THREE.Vector3(-1,-1,0));
+			squareGeometry.vertices.push(new THREE.Vector3(1,-1,0));
+			squareGeometry.vertices.push(new THREE.Vector3(1,1,0));
+			squareGeometry.vertices.push(new THREE.Vector3(-1,1,0));
 			squareGeometry.faces.push(new THREE.Face4(0,1,2,3));
-			squareGeometry.faceVertrxUvs[0].push([
+			squareGeometry.faceVertexUvs[0].push([
 				new THREE.UV(0,0,0.0),
 				new THREE.UV(1,0,0.0),
 				new THREE.UV(0,0,1.0),
@@ -62,12 +62,44 @@
 
 			scene.add(squareMesh);
 
-			star.[i] = new Object();
-			star.[i].angle = 0.0;
-			star.[i].r = Math.random();
-			star.[i].g = Math.random();
-			star.[i].b = Math.random();
-			star.[i].mesh = squareMesh;
+			star[i] = new Object();
+			star[i].angle = 0.0;
+			star[i].r = Math.random();
+			star[i].g = Math.random();
+			star[i].b = Math.random();
+			star[i].mesh = squareMesh;
+		}
+
+		function animeteScene(){
+			requestAnimationFrame(animeteScene);
+			for(i=0; i<num; i++){
+				spin += Math.PI * 2 /num;
+
+				if(spin >(Math.PI*2)){
+					spin = 0;
+					star[i].angle += i/num;
+					star[i].dist -=0.01;
+
+					if(star[i].dist < 0.0){
+						star[i].dist +=0.5;
+						star[i].r = Math.random();
+						star[i].g = Math.random();
+						star[i].b = Math.random();
+						
+					}
+				}
+				star[i].mesh.matrix.setPosition(new THREE.Vector3(star[i].dist,0,0));
+				var mr = THREE.Matrix4();
+				mr.makeRotationZ(spin);
+				star[i].mesh.applyMatrix(mx);
+
+				star[i].mesh.material.color.setRGB(star[i].r, star[i].g,star[i].b);
+			}
+			rendererScene();
+		}
+
+		function rendererScene(){
+			animeteScene();
 		}
 	}
 
